43c6d852654eb90aa0a95adea38ac7dd28965fb2
Nicole <nicoleshek315@gmail.com>
2020-08-16 19:06:10 +0200

    saved todo items to mongodb database

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js')
 
 const app = express();
-let items = ['eat food'];
-let workItems = [];
+// let items = ['eat food'];
+// let workItems = [];
 
 //using ejs as view engine
 app.set('view engine', 'ejs');
@@ -14,6 +15,30 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static('public'));
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/todolistDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+    name: 'eat food'
+});
+
+const item2 = new Item({
+    name: 'drink water'
+});
+
+
+const defaultItems = [item1, item2];
+
 
 app.get('/', (req, res) => {
 
@@ -27,34 +52,54 @@ app.get('/', (req, res) => {
 
     let day = today.toLocaleDateString("en-US", options);
 
-    res.render('list', {
-        listTitle: day,
-        newListItems: items
+    Item.find({}, function (err, myItems) {
+        if (myItems.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("successfully added default items");
+                }
+                res.redirect("/");
+            });
+        } else {
+            console.log(myItems);
+            res.render('list', {
+                listTitle: day,
+                newListItems: myItems
+            });
+
+        }
     });
+
 });
 
 app.post('/', function (req, res) {
 
-    let item = req.body.newItem;
+    let itemName = req.body.newItem;
 
-    if (req.body.list === 'Work') {
-        workItems.push(item);
-        res.redirect('/work');
-    } else {
-        items.push(item);
-        console.log(items);
-        res.redirect('/');
-    }
+    const item = new Item({
+        name: itemName
+    });
+
+    item.save();
+    res.redirect("/");
 
 });
 
+app.post('/delete', function (req, res) {
+    const checkedBoxId = req.body.checkbox;
 
-app.get('/work', function (req, res) {
-    res.render('list', {
-        listTitle: 'Work List',
-        newListItems: workItems
+    Item.findByIdAndRemove(checkedBoxId, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
     });
 });
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
